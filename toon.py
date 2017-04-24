4385ecb15a8300b9c26066eaa7086fb951866058
krocat <paul@sesha.net>
2017-04-24 11:12:04 +0200

    Updated to toonlib 0.1.3

--- toon.py
+++ toon.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.discovery import load_platform
 import homeassistant.helpers.config_validation as cv
 
 # Home Assistant depends on 3rd party packages for API specific code.
-REQUIREMENTS = ['toonlib==0.1.2']
+REQUIREMENTS = ['toonlib==0.1.3']
 
 _LOGGER = logging.getLogger(__name__)
 
