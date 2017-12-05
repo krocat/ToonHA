b07027a4bb99bbaf4459a2b008b60b27f3304800
Michiel Lowijs <mlowijs@gmail.com>
2017-12-05 15:25:54 +0100

    Added supported_flags

--- climate/toon.py
+++ climate/toon.py
@@ -11,13 +11,16 @@ from homeassistant.components.climate import (ClimateDevice,
                                               STATE_PERFORMANCE,
                                               STATE_HEAT,
                                               STATE_ECO,
-                                              STATE_COOL)
+                                              STATE_COOL,
+                                              SUPPORT_TARGET_TEMPERATURE,
+                                              SUPPORT_OPERATION_MODE)
 from homeassistant.const import TEMP_CELSIUS
 
 import custom_components.toon as toon_main
 
 
 _LOGGER = logging.getLogger(__name__)
+SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup thermostat."""
@@ -42,6 +45,11 @@ class ThermostatDevice(ClimateDevice):
                                 STATE_HEAT,
                                 STATE_ECO,
                                 STATE_COOL]
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
 
     @property
     def name(self):
